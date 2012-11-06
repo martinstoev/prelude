60ef36217804ad9757729c7d97e3fde88a253794
Martin Stoev <dev@martinstoev.de>
2012-11-06 18:38:19 +0100

    Changed font-size and color theme to Tango.

--- personal/misc.el
+++ personal/misc.el
@@ -2,3 +2,13 @@
 ;; Setting rbenv path
 (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
 (setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
+
+;; Setting default font-size
+(when window-system
+  (set-face-attribute 'default nil :height 160))
+
+;; Setting color theme
+(disable-theme 'zenburn)
+(load-theme 'tango-dark)
+
+(global-linum-mode 1)
